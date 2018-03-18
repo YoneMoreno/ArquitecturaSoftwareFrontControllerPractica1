6f25d650d2b9731995dba4aeb6ca1fa75d3f8ea2
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-18 18:03:17 +0000

    Add a log stored in alocal file with:
    List<String> lines = Arrays.asList("ConviertePuntosNota::convertidor::el usuario introdujo: " + evaluacion);
                Path file = Paths.get("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\ConviertePuntosNota.log.txt");
                Files.write(file, lines, Charset.forName("UTF-8"));
    
    TODO: Discover how to append the new contents into the file. TODO: Know how to solve this exception:
    
    Advertencia:   Unable to restore sessions for web module [/ASAPLICACIONCURSOSPRACTICA1] from previous deployment
    java.io.EOFException

--- src/java/beans/ConviertePuntosNota.java
+++ src/java/beans/ConviertePuntosNota.java
@@ -5,7 +5,17 @@
  */
 package beans;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import java.io.File;
 
 /**
  *
@@ -16,8 +26,19 @@ public class ConviertePuntosNota {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-    
-    public String convertidor(int evaluacion){
-        return evaluacion >= 5 ? "Apto" : "No apto";
+    public String convertidor(int evaluacion) {
+
+        try {
+
+            List<String> lines = Arrays.asList("ConviertePuntosNota::convertidor::el usuario introdujo: " + evaluacion);
+            Path file = Paths.get("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\ConviertePuntosNota.log.txt");
+            Files.write(file, lines, Charset.forName("UTF-8"));
+
+            return evaluacion >= 5 ? "Apto" : "No apto";
+
+        } catch (IOException ex) {
+            Logger.getLogger(ConviertePuntosNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "ERROR";
     }
 }
