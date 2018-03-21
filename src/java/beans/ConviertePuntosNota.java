9e136d136f21db59565d9020ebb683b27f28469b
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-21 08:56:03 +0000

    Add PreDestroy and PostConstruct to ConviertePuntosNota

--- src/java/beans/ConviertePuntosNota.java
+++ src/java/beans/ConviertePuntosNota.java
@@ -7,11 +7,15 @@ import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import java.io.File;
 import java.io.FileWriter;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Startup;
 
+@Startup
 @Stateless
 public class ConviertePuntosNota {
 
-    File file = new File("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\ConviertePuntosNota.log.txt");
+    File file = new File("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\log.txt");
 
     public String convertidor(int evaluacion) {
 
@@ -26,6 +30,29 @@ public class ConviertePuntosNota {
         }
 
         return "ERROR";
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+
+        try {
+            String text = "ConviertePuntosNota::postConstruct::el metodo es void \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(ConviertePuntosNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        try {
+            String text = "ConviertePuntosNota::preDestroy::el metodo preDestroy es void \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(ConviertePuntosNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     private void writeLogToFile(String text, File file) throws IOException {
