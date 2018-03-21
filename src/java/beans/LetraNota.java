3590a552e4daef9ab3466616db12f3baee53a6e4
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-21 09:07:39 +0000

    Add logs to LetraNota, PreDestroy and PostConstruct

--- src/java/beans/LetraNota.java
+++ src/java/beans/LetraNota.java
@@ -5,6 +5,14 @@
  */
 package beans;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 
 /**
@@ -14,9 +22,48 @@ import javax.ejb.Stateless;
 @Stateless
 public class LetraNota {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-    public String convierteLetraNota(String letra){
-        return letra.equals("F") ? "No apto" : letra.equals("E") ? "No apto" : "Apto";
+    File file = new File("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\log.txt");
+
+    public String convierteLetraNota(String letra) {
+
+        try {
+            String text = "LetraNota::convierteLetraNota::el usuario introdujo la LETRA: " + letra + "\n";
+            writeLogToFile(text, file);
+
+            return letra.equals("F") ? "No apto" : letra.equals("E") ? "No apto" : "Apto";
+        } catch (IOException ex) {
+            Logger.getLogger(LetraNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "ERROR";
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        try {
+            String text = "LetraNota::postConstruct:: nuestro metodo PostConstruct es void!!!!!!!! \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(LetraNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        try {
+            String text = "LetraNota::preDestroy:: el metodo PreDestroy es void!!!!!!!!!!! \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(LetraNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void writeLogToFile(String text, File file) throws IOException {
+
+        BufferedWriter output = null;
+        output = new BufferedWriter(new FileWriter(file, true));
+        output.write(text);
+        output.close();
     }
 }
