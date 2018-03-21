a73b79dfe2841eb597a916912a70edee41b4c0a7
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-21 09:03:12 +0000

    Fix bug where if we wrote a comment and "Pass" as a mark, the check of comment's length > 5 was always succeding. Also as PreDestroy and PostConstruct to ComentarioNota

--- src/java/beans/ComentarioNota.java
+++ src/java/beans/ComentarioNota.java
@@ -5,6 +5,14 @@
  */
 package beans;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 
 /**
@@ -14,13 +22,52 @@ import javax.ejb.Stateless;
 @Stateless
 public class ComentarioNota {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    File file = new File("C:\\Users\\YonePC\\Videos\\ASAPLICACIONCURSOSPRACTICA1\\src\\java\\beans\\log.txt");
+
     public String convierteComentarioNota(String evaluacion, String comentario) {
-        if (evaluacion.trim().equals("Apto") && comentario != null && comentario.length() > 5) {
-            return "Apto";
-        } else {
-            return "No Apto";
+
+        try {
+            String text = "ConvierteComentarioNota::convierteComentarioNota::el usuario introdujo la evaluacion: " + evaluacion
+                    + " y el comentario: " + comentario + "\n";
+            writeLogToFile(text, file);
+
+            if (evaluacion.equals("Apto") && comentario != null && comentario.trim().length() > 5) {
+                return "Apto";
+            } else {
+                return "No Apto";
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ComentarioNota.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return "ERROR";
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        try {
+            String text = "ConvierteComentarioNota::postConstruct::el metodo postConstruct es void \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(ComentarioNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        try {
+            String text = "ConvierteComentarioNota::preDestroy::el metodo preDestroy es void!!! \n";
+            writeLogToFile(text, file);
+        } catch (IOException ex) {
+            Logger.getLogger(ComentarioNota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void writeLogToFile(String text, File file) throws IOException {
+
+        BufferedWriter output = null;
+        output = new BufferedWriter(new FileWriter(file, true));
+        output.write(text);
+        output.close();
     }
 }
