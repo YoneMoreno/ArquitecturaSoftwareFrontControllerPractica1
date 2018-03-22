f6e323228ccbbd0f29eaa4bb47c85cf27529c0ea
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-22 09:22:05 +0000

    AlumnosCommand::process() Información:   Cannot serialize session attribute evaluaciones for session d02248eb65c0dedfe2ddf33264d3 java.io.NotSerializableException: org.Evaluacion

--- src/java/frontController/FrontCommand.java
+++ src/java/frontController/FrontCommand.java
@@ -1,11 +1,16 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package frontController;
 
+import beans.SingletonFuncionLog;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,7 +27,10 @@ public abstract class FrontCommand {
     protected HttpServletRequest request;
     protected HttpServletResponse response;
 
-    public void init(ServletContext context, HttpServletRequest request, HttpServletResponse response) {
+    public void init(ServletContext context, HttpServletRequest request, HttpServletResponse response) throws NamingException {
+        SingletonFuncionLog singletonFuncionLog5 = InitialContext.doLookup("java:global/ASAPLICACIONCURSOSPRACTICA1/SingletonFuncionLog");
+
+        singletonFuncionLog5.funcionLog("FrontCommand", "init");
         this.context = context;
         this.request = request;
         this.response = response;
@@ -31,7 +39,14 @@ public abstract class FrontCommand {
     abstract public void process(HttpServletRequest request);
 
     public void forward(String target) throws ServletException, IOException {
-        RequestDispatcher dp = context.getRequestDispatcher(target);
-        dp.forward(request, response);
+        try {
+            SingletonFuncionLog singletonFuncionLog5 = InitialContext.doLookup("java:global/ASAPLICACIONCURSOSPRACTICA1/SingletonFuncionLog");
+            
+            singletonFuncionLog5.funcionLog("FrontCommand", "forward");
+            RequestDispatcher dp = context.getRequestDispatcher(target);
+            dp.forward(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(FrontCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
