5ac03e4eb7f7e1684a9418d1fb0aea4ea2cade01
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 14:55:01 +0100

    Crear AsignaturaProfesor statefull bean and Tema stateless bean

---
+++ src/java/beans/AsignaturaProfesor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author YonePC
+ */
+@Stateful
+public class AsignaturaProfesor {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    String nombre;
+    ArrayList<Tema> lista;
+
+    public AsignaturaProfesor() {
+    }
+
+    public AsignaturaProfesor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Tema> getLista() {
+        return lista;
+    }
+    
+    
+}
