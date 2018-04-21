0b74f1097e9c249653a0a29407c98286c3735745
Yone <yone.moreno101@alu.ulpgc.es>
2018-04-21 14:20:02 +0100

    Add Entity Profesor.java in org

---
+++ src/java/org/Profesor.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author YonePC
+ */
+@Entity
+@Table(name = "profesor")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Profesor.findAll", query = "SELECT p FROM Profesor p")
+    , @NamedQuery(name = "Profesor.findByIdProfesor", query = "SELECT p FROM Profesor p WHERE p.idProfesor = :idProfesor")
+    , @NamedQuery(name = "Profesor.findByNombre", query = "SELECT p FROM Profesor p WHERE p.nombre = :nombre")
+    , @NamedQuery(name = "Profesor.findByContrasena", query = "SELECT p FROM Profesor p WHERE p.contrasena = :contrasena")
+    , @NamedQuery(name = "Profesor.findByAsignatura", query = "SELECT p FROM Profesor p WHERE p.asignatura = :asignatura")
+    , @NamedQuery(name = "Profesor.findByValoracion", query = "SELECT p FROM Profesor p WHERE p.valoracion = :valoracion")
+    , @NamedQuery(name = "Profesor.findByCorreo", query = "SELECT p FROM Profesor p WHERE p.correo = :correo")
+    , @NamedQuery(name = "Profesor.findByTelefono", query = "SELECT p FROM Profesor p WHERE p.telefono = :telefono")
+    , @NamedQuery(name = "Profesor.findByDespacho", query = "SELECT p FROM Profesor p WHERE p.despacho = :despacho")})
+public class Profesor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idProfesor")
+    private Integer idProfesor;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "contrasena")
+    private String contrasena;
+    @Basic(optional = false)
+    @Column(name = "asignatura")
+    private String asignatura;
+    @Basic(optional = false)
+    @Column(name = "valoracion")
+    private int valoracion;
+    @Basic(optional = false)
+    @Column(name = "correo")
+    private String correo;
+    @Basic(optional = false)
+    @Column(name = "telefono")
+    private int telefono;
+    @Basic(optional = false)
+    @Column(name = "despacho")
+    private String despacho;
+
+    public Profesor() {
+    }
+
+    public Profesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public Profesor(Integer idProfesor, String nombre, String contrasena, String asignatura, int valoracion, String correo, int telefono, String despacho) {
+        this.idProfesor = idProfesor;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.asignatura = asignatura;
+        this.valoracion = valoracion;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.despacho = despacho;
+    }
+
+    public Integer getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(String asignatura) {
+        this.asignatura = asignatura;
+    }
+
+    public int getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(int valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDespacho() {
+        return despacho;
+    }
+
+    public void setDespacho(String despacho) {
+        this.despacho = despacho;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProfesor != null ? idProfesor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Profesor)) {
+            return false;
+        }
+        Profesor other = (Profesor) object;
+        if ((this.idProfesor == null && other.idProfesor != null) || (this.idProfesor != null && !this.idProfesor.equals(other.idProfesor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.Profesor[ idProfesor=" + idProfesor + " ]";
+    }
+    
+}
