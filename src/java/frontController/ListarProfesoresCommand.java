2497b373ee059ade715b3b23ad8cc4e6d0d4d66f
Yone <yone.moreno101@alu.ulpgc.es>
2018-04-29 07:45:25 +0100

    We get all profesors with criteria api

--- src/java/frontController/ListarProfesoresCommand.java
+++ src/java/frontController/ListarProfesoresCommand.java
@@ -1,10 +1,15 @@
 package frontController;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import jpa.Profesor_1Facade;
+import org.Profesor_1;
 
 /**
  *
@@ -15,6 +20,18 @@ public class ListarProfesoresCommand extends FrontCommand {
     @Override
     public void process(HttpServletRequest request) {
         try {
+
+            try {
+                Profesor_1Facade profesorFacade
+                        = InitialContext.doLookup("java:global/ASAPLICACIONCURSOSPRACTICA1/Profesor_1Facade");
+                List<Profesor_1> allProfesors = profesorFacade.findAllWithCriteria();
+                request.setAttribute("allProfesors", allProfesors);
+                //System.out.println("AllProfesors number is: " + allProfesors.size());
+
+            } catch (NamingException ex) {
+                Logger.getLogger(ListarProfesoresCommand.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
             forward("/Public/ListaProfesores.jsp");
         } catch (ServletException ex) {
             Logger.getLogger(ListarProfesoresCommand.class.getName()).log(Level.SEVERE, null, ex);
--- src/java/jpa/Profesor_1Facade.java
+++ src/java/jpa/Profesor_1Facade.java
@@ -6,9 +6,14 @@
 package jpa;
 
 import frontController.AbstractFacade;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.Profesor_1;
 
 /**
@@ -29,5 +34,15 @@ public class Profesor_1Facade extends AbstractFacade<Profesor_1> {
     public Profesor_1Facade() {
         super(Profesor_1.class);
     }
-    
+
+    public List<Profesor_1> findAllWithCriteria() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Profesor_1> criteriaQuery = criteriaBuilder.createQuery(Profesor_1.class);
+        Root<Profesor_1> profesorTable = criteriaQuery.from(Profesor_1.class);
+        criteriaQuery.select(profesorTable);
+        TypedQuery<Profesor_1> query = em.createQuery(criteriaQuery);
+        List<Profesor_1> allProfesors = query.getResultList();
+        return allProfesors;
+    }
+
 }
