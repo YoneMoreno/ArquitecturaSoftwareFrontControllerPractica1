70ad2d2373915e32bb0ee8153ba5ae78f538f3b6
Yone <yone.moreno101@alu.ulpgc.es>
2018-05-02 23:00:11 +0100

    Add button, command and jsp to sendMessages

---
+++ src/java/frontController/SendMessageCommand.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frontController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author YonePC
+ */
+public class SendMessageCommand extends FrontCommand {
+
+    @Override
+    public void process(HttpServletRequest request) {
+        try {
+            forward("/Public/SendMessageForm.jsp");
+        } catch (ServletException ex) {
+            Logger.getLogger(SendMessageCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SendMessageCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ web/Public/SendMessageForm.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : SendMessageForm
+    Created on : 02-may-2018, 22:59:12
+    Author     : YonePC
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
